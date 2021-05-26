515a2ed84fbd6910dde20ae72c6f7cc1d667f350
Rahul kucharla <60927178+Rahulkucharla@users.noreply.github.com>
2021-05-26 11:36:45 +0530

    Update Week1A1.java

--- week1/Week1A1.java
+++ week1/Week1A1.java
@@ -1,6 +1,10 @@
 class Week1A1 {
 
   public static void main(String[] args) {
-    // code...
+    String a,b;
+        a=args[0];
+        b=args[1];
+        System.out.println(a+" and "+b);
   }
 }
+
