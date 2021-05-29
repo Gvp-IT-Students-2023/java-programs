0113127b4b31af7931b6931004dfe44ecefec76b
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2021-05-29 14:33:52 +0530

    Changes

--- week4/Week4A.java
+++ week4/Week4A.java
@@ -19,15 +19,13 @@ class Week4A {
 
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-
     System.out.print("Enter width of box: ");
     int w = sc.nextInt();
     System.out.print("Enter height of box: ");
     int h = sc.nextInt();
     System.out.print("Enter depth of box: ");
     int d = sc.nextInt();
-
     Box box = new Box(w, h, d);
-    System.out.println("Volume of box is "+ box.getVolume()+" sq.units");
+    System.out.println("Volume of box is " + box.getVolume() + " sq.units");
   }
 }
--- week4/Week4B.java
+++ week4/Week4B.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 class Calculator {
 
-  public static int powerInt(int num1, int num2) {
+  static int powerInt(int num1, int num2) {
     int pow = 1;
     for (int i = 0; i < num2; i++) pow *= num1;
     return pow;
   }
 
-  public static double powerDouble(double num1, double num2) {
+  static double powerDouble(double num1, double num2) {
     double pow = 1.0;
     for (int i = 0; i < num2; i++) pow *= num1;
     return pow;
@@ -24,16 +24,17 @@ class Week4B {
     int n1 = sc.nextInt();
     System.out.print("Enter exponent integer: ");
     int p1 = sc.nextInt();
-    System.out.println(
-      n1 + " power " + p1 + " is " + Calculator.powerInt(n1, p1)
-    );
+
+    int pw1 = Calculator.powerInt(n1, p1);
+    System.out.println(n1 + " power " + p1 + " is " + pw1);
 
     System.out.print("Enter base double: ");
     double n2 = sc.nextDouble();
     System.out.print("Enter exponent double: ");
     double p2 = sc.nextDouble();
-    System.out.println(
-      n2 + " power " + p2 + " is " + Calculator.powerDouble(n2, p2)
-    );
+
+    double pw2 = Calculator.powerDouble(n2, p2);
+    System.out.println(n2 + " power " + p2 + " is " + pw2);
+    
   }
 }
