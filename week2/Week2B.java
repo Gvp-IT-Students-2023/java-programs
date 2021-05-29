d708abfbaf1f8d9cf5f1bc5b75f69154100cc9aa
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2021-05-29 13:36:02 +0530

    Updates Week2 programs

--- week2/Week2B.java
+++ week2/Week2B.java
@@ -1,9 +1,38 @@
 import java.util.Scanner;
 
 class Week2B {
+
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-  
-    // changes
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    boolean found = false;
+    for (int i = 0; i < n; i++) {
+      int curr = arr[i];
+      if (isOdd(curr) && isPrime(curr)) {
+        System.out.println("Found :" + curr);
+        if (!found) found = true;
+      }
+    }
+
+    if (!found) {
+      System.out.println("Not Found");
+    }
+  }
+
+  static boolean isOdd(int a) {
+    return a % 2 == 1;
+  }
+
+  static boolean isPrime(int num) {
+    for (int i = 2; i <= num / 2; i++) {
+      if (num % i == 0) return false;
+    }
+
+    return true;
   }
 }
---
+++ week2/Week2C.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Week2C {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    // Incomplete
+    // 1. Find Frequency
+    // 2. Print in frequency order
+  }
+}
