80ebc35ee5a0c67cf8db13abb01ceb5c14e6f2dd
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2021-05-31 09:23:56 +0530

    Update: Week5B

--- week5/Week5B.java
+++ week5/Week5B.java
@@ -2,8 +2,34 @@ import java.util.Scanner;
 
 class Week5B {
 
+  static String vowels = "AEIOUaeiou";
+
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    //  complete the code
+    int n = sc.nextInt();
+    String[] strings = new String[n];
+
+    for (int i = 0; i < n; i++) {
+      strings[i] = sc.next();
+    }
+
+    for (int i = 0; i < n; i++) {
+      int[] res = getVowelsConsonantsCount(strings[i]);
+      System.out.print(strings[i] + ":");
+      System.out.print(" vowels - " + res[0]);
+      System.out.println(" consonants - " + res[1]);
+    }
+  }
+
+  static int[] getVowelsConsonantsCount(String s) {
+    int vowelsCount = 0, consonantsCount = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char t = s.charAt(i);
+      if (Character.isLetter(t)) {
+        if (vowels.indexOf(t) != -1) vowelsCount++; else consonantsCount++;
+      }
+    }
+
+    return new int[] { vowelsCount, consonantsCount };
   }
 }
