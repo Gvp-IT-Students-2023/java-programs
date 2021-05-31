21353ad743f19cd867cb79428b89bd1596810ba4
Surya Chandra Pavan Adurthi <p2pdops@gmail.com>
2021-05-31 09:08:12 +0530

    Updated Week5C.java

--- week5/Week5C.java
+++ week5/Week5C.java
@@ -4,6 +4,31 @@ class Week5C {
 
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    //  complete the code
+    String s1 = sc.next();
+    String s2 = sc.next();
+    char[] c1 = sort(s1.toCharArray());
+    char[] c2 = sort(s2.toCharArray());
+
+    boolean anagram = true;
+    for (int i = 0; i < c1.length; i++) {
+      if (c1[i] != c2[i]) anagram = false;
+    }
+
+    System.out.print(s1 + " and " + s2 + " are");
+    String res = (anagram ? " anagrams" : " not anagrams");
+    System.out.print(res);
+  }
+
+  static char[] sort(char[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          char t = arr[j];
+          arr[j] = arr[i];
+          arr[i] = t;
+        }
+      }
+    }
+    return arr;
   }
 }
